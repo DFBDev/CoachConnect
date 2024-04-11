ebf9968c9e487c595c50461efbff0c2fa72dbf0b
Dementexo <browndon962@gmail.com>
2024-04-11 16:04:34 -0500

    Added forms and request handler.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,15 @@ function App() {
 
   return (
     <>
-      <p>testing</p>
+    <form id="infoForm">
+      <label htmlFor={"test1"}>Test1:</label>
+      <input type={"text"} name={"test1"} id={'test1'}></input>
+      <label htmlFor={"test2"}>Test2:</label>
+      <input type={"text"} name={"test2"} id={'test2'}></input>
+      <label htmlFor={"test3"}>Test3:</label>
+      <input type={"text"} name={"test3"} id={'test3'}></input>
+      <input type={"submit"} name={"submitButton"} id={"submitButton"}></input>
+    </form>
     </>
   )
 }

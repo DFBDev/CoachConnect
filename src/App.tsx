fad2d3f99d3ad099a111500fe6717d65da65a7f6
Dementexo <browndon962@gmail.com>
2024-05-29 17:30:31 -0500

    Changed and increased form data options.

--- src/App.tsx
+++ src/App.tsx
@@ -15,13 +15,15 @@ function App() {
       
       urlEncoded.append("lastName", data.get("lastName")!.toString());
       urlEncoded.append("firstName", data.get("firstName")!.toString());
-      urlEncoded.append("description", data.get("description")!.toString());
+      urlEncoded.append("dialcount", data.get("dialcount")!.toString());
+      urlEncoded.append("conversationcount", data.get("conversationcount")!.toString());
+      urlEncoded.append("appointmentsset", data.get("appointmentsset")!.toString());
+      urlEncoded.append("appointmentscomplete", data.get("appointmentscomplete")!.toString());
+      urlEncoded.append("timestamp", data.get("timestamp")!.toString());
       
-      const res = await fetch("https://ts-be.onrender.com/actions/T2zOwRu-iji-RinOje*h", {method: "POST", body: urlEncoded});
+      await fetch("https://ts-be.onrender.com/actions/T2zOwRu-iji-RinOje*h", {method: "POST", body: urlEncoded});
       formElement.reset();
       submitButtonHolder!.innerHTML = "<input type=\"submit\" name=\"submitButton\" id=\"submitButton\"></input>";
-      
-      console.log(res);
     })
   }, [])
 
@@ -33,8 +35,16 @@ function App() {
       <input type={"text"} name={"lastName"} id={'lastName'}></input>
       <label htmlFor={"firstName"}>First Name</label>
       <input type={"text"} name={"firstName"} id={'firstName'}></input>
-      <label htmlFor={"description"}>Description</label>
-      <textarea id='description' name='description'></textarea>
+      <label htmlFor={"dialcount"}>Dial Count</label>
+      <input type={"number"} name={"dialcount"} id={'dialcount'}></input>
+      <label htmlFor={"conversationcount"}>Conversation Count</label>
+      <input type={"number"} name={"conversationcount"} id={'conversationcount'}></input>
+      <label htmlFor={"appointmentsset"}>Appointments Set</label>
+      <input type={"number"} name={"appointmentsset"} id={'appointmentsset'}></input>
+      <label htmlFor={"conversationcount"}>Appointments Complete</label>
+      <input type={"number"} name={"appointmentscomplete"} id={'appointmentscomplete'}></input>
+      <label htmlFor={"timestamp"}>Current Date/Time</label>
+      <input type={"datetime-local"} name={"timestamp"} id={'timestamp'}></input>
       <div id='submitHolder'>
         <input type={"submit"} name={"submitButton"} id={"submitButton"}></input>
       </div>
